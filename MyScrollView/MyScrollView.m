5d2ef3ee34f52725b00000b432a0c224e7eeaf26
Jaison Bhatti <jaison.bhatti@gmail.com>
2017-09-18 17:41:42 -0400

    add 4 subviews with boxes

---
+++ MyScrollView/MyScrollView.m
@@ -0,0 +1,26 @@
+//
+//  MyScrollView.m
+//  MyScrollView
+//
+//  Created by Jaison Bhatti on 2017-09-18.
+//  Copyright © 2017 Jaison Bhatti. All rights reserved.
+//
+
+#import "MyScrollView.h"
+
+@implementation MyScrollView
+
+
+-(void)handlePan:(UIPanGestureRecognizer*) panGesture {
+    
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
--- MyScrollView/ViewController.m
+++ MyScrollView/ViewController.m
@@ -10,20 +10,44 @@
 
 @interface ViewController ()
 
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIView *myView = [[UIView alloc] initWithFrame:CGRectZero];
+    [self.view addSubview:myView];
+    myView.frame = myView.superview.bounds;
+    
+    UIView *myView1 = [[UIView alloc] initWithFrame:CGRectMake(20, 20, 100, 100)];
+    myView1.backgroundColor = [UIColor redColor];
+    [myView addSubview:myView1];
+    
+    UIView *myView2 = [[UIView alloc] initWithFrame:CGRectMake(150, 150, 150, 200)];
+    myView2.backgroundColor = [UIColor greenColor];
+    [myView addSubview:myView2];
+    
+    UIView *myView3 = [[UIView alloc] initWithFrame:CGRectMake(40, 400, 200, 150)];
+    myView3.backgroundColor = [UIColor blueColor];
+    [myView addSubview:myView3];
+
+    UIView *myView4 = [[UIView alloc] initWithFrame:CGRectMake(100, 600, 180, 150)];
+    myView4.backgroundColor = [UIColor yellowColor];
+    [myView addSubview:myView4];
+    
+}
+
+-(void)viewDidAppear:(BOOL)animated {
+    CGRect rect = self.view.bounds;
+    rect.origin = CGPointMake(0, 100);
+    self.view.bounds = rect;
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 
 @end
