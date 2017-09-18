8b904ff104d90fd8c852f35856ab11df42a3e87a
Jaison Bhatti <jaison.bhatti@gmail.com>
2017-09-18 12:57:31 -0400

    first commit

---
+++ MyScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyScrollView
+//
+//  Created by Jaison Bhatti on 2017-09-18.
+//  Copyright © 2017 Jaison Bhatti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
